9e0ddeb6b3feedceda390d7922cef730f6c99f3e
DarkNight <57034210+BeastGamer81@users.noreply.github.com>
2022-08-24 20:12:07 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,7 +20,8 @@ else
 
         echo "[Heliactyl Egg] Heliactyl files are downloaded. For reverse proxy(Nginx) setup please visit the docs: https://gitlab.com/heliactyl/panel/-/blob/master/README.md";
         exit 0;
-   then
+        ;;
+then
         exit 0;
         ;;
     "N"|"n")
